88cc02cc240c4210411098b973336f1077a74e1e
Elshanmirza <elshan.mirzayev95@gmail.com>
2022-12-13 22:58:10 +0400

    last

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ let leftSpan = document.querySelector('.left span')
 let rightSpan = document.querySelector('.right span')
 let inputFirst = document.querySelector('.first')
 let inputSecond = document.querySelector('.second')
-
+inputFirst.value = '1'
 
 lists.forEach(lists => {
     lists.addEventListener('click', function (e) {
-        // inputFirst.addEventListener('keydown', function (e) {
+
         const selectedList = e.target
         const side = selectedList.parentElement.parentElement
         // console.log(side)
@@ -18,28 +18,36 @@ lists.forEach(lists => {
         let rightUnit = document.querySelector('.right .blueviolet')
 
 
-        
-            fetch(`https://api.exchangerate.host/latest?base=${leftUnit.innerText}&symbols=${rightUnit.innerText}`)
-                .then(response => response.json())
-                .then(res => {
 
-                    let exg = res.rates[rightUnit.innerText]
-                    leftSpan.innerText = `1 ${leftUnit.innerText} = ${exg} ${rightUnit.innerText}`
-                    inputFirst.value = inputSecond.value * exg
-                })
+        fetch(`https://api.exchangerate.host/latest?base=${leftUnit.innerText}&symbols=${rightUnit.innerText}`)
+            .then(response => response.json())
+            .then(res => {
 
-            fetch(`https://api.exchangerate.host/latest?base=${rightUnit.innerText}&symbols=${leftUnit.innerText}`)
-                .then(response => response.json())
-                .then(res => {
-                    let exg = res.rates[leftUnit.innerText]
-                    rightSpan.innerText = `1 ${rightUnit.innerText} = ${exg} ${leftUnit.innerText}`
-                    inputSecond.value = inputFirst.value * exg
-                })
+                let exg = res.rates[rightUnit.innerText]
+                leftSpan.innerText = `1 ${leftUnit.innerText} = ${exg} ${rightUnit.innerText}`
+                // inputFirst.addEventListener('keydown', function (e) {
+                // inputFirst.value = '1'
+                inputSecond.value = inputFirst.value * exg
+                // Math.floor(inputSecond.value) = inputFirst.value * exg
 
-        // }, false);
-        
+                // }, false);
+            })
+
+        fetch(`https://api.exchangerate.host/latest?base=${rightUnit.innerText}&symbols=${leftUnit.innerText}`)
+            .then(response => response.json())
+            .then(res => {
+                let exg = res.rates[leftUnit.innerText]
+                rightSpan.innerText = `1 ${rightUnit.innerText} = ${exg} ${leftUnit.innerText}`
+
+                inputFirst.value = inputSecond.value * exg
+            })
+
+
+
+
+
     })
-    
+
 })
 
 window(onload)
