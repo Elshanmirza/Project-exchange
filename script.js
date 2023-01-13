a8f4405115603c77def7a6d88d3155fa48ed2408
Elshanmirza <elshan.mirzayev95@gmail.com>
2023-01-13 16:17:12 +0400

    last

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+window.addEventListener('offline', (e) => { console.log('offline'); });
 let lists = document.querySelectorAll('li')
 let leftSpan = document.querySelector('.left span')
 let rightSpan = document.querySelector('.right span')
 let inputFirst = document.querySelector('.first')
 let inputSecond = document.querySelector('.second')
-inputFirst.value = '1'
+var GLOBAL_BASE, GlOBAL_SYMBOL;
 
 lists.forEach(lists => {
     lists.addEventListener('click', function (e) {
@@ -24,10 +25,12 @@ lists.forEach(lists => {
             .then(res => {
 
                 let exg = res.rates[rightUnit.innerText]
+                GLOBAL_BASE = exg;
+                GlOBAL_SYMBOL = 1 / exg;
                 leftSpan.innerText = `1 ${leftUnit.innerText} = ${exg} ${rightUnit.innerText}`
                 // inputFirst.addEventListener('keydown', function (e) {
                 // inputFirst.value = '1'
-                inputSecond.value = inputFirst.value * exg
+                inputSecond.value = (inputFirst.value * exg).toFixed(2)
                 // Math.floor(inputSecond.value) = inputFirst.value * exg
 
                 // }, false);
@@ -37,9 +40,11 @@ lists.forEach(lists => {
             .then(response => response.json())
             .then(res => {
                 let exg = res.rates[leftUnit.innerText]
+                GLOBAL_BASE = exg;
+                GlOBAL_SYMBOL = 1 / exg;
                 rightSpan.innerText = `1 ${rightUnit.innerText} = ${exg} ${leftUnit.innerText}`
 
-                inputFirst.value = inputSecond.value * exg
+                inputFirst.value = (inputSecond.value * exg).toFixed(0)
             })
 
 
